807529de90f20d39a72c3eb1b1cb2b31eb2193c6
walmir91 <63596267+walmir91@users.noreply.github.com>
2020-04-22 21:31:47 -0300

    Update dc.js

--- dc.js
+++ dc.js
@@ -1,20 +1,27 @@
-function calcularEstrelas(){
+function calcularEstrelas() {
     var inFragmentos = document.getElementById('inFragmentos');
     var inStar = document.getElementById('inStar');
     var outResposta = document.getElementById('outResposta');
     var iniFrag = Number(inFragmentos.value);
     var iniStar = Number(inStar.value);
-    var estrelas = [10, 25, 50, 90, 150, 240, 370, 550, 790, 1100];
+    var estrelas = [10, 15, 25, 40, 60, 90, 130, 180, 240, 310];
     var frag = 0;
     var n = estrelas[(iniStar)];
     frag = iniFrag;
-    while(frag > n){
+    while (frag >= n) {
         frag = frag - n;
         iniStar = iniStar + 1;
         n = estrelas[(iniStar)];
     }
-    outResposta.textContent = "Seu heroi ficará com: " + estrelas[(iniStar)];
-
+    if (iniStar > 9) {
+        outResposta.textContent = 'Seu personagem ficará lendário ' + (iniStar / 2) + ' Estrela(s).';
+    }
+    if (iniStar > 5 && iniStar <= 9) {
+        outResposta.textContent = 'Seu personagem ficará lendário ' + (iniStar - 5) + ' Estrela(s).';
+    }
+    if(iniStar <= 5){
+        outResposta.textContent = 'Seu personagem ficará com: ' + iniStar + ' Estrela(s).';
+     }
 }
 var btCalcular = document.getElementById('btCalcular');
 btCalcular.addEventListener('click', calcularEstrelas);
